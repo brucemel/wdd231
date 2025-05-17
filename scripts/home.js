1793991da0bf52306452415dc4240b497089bef2
brucemel <mel24004@byui.edu>
2025-05-16 19:14:34 -0500

    boxone

--- scripts/home.js
+++ scripts/home.js
@@ -14,3 +14,32 @@ const today = new Date();
 
 currentyear.innerHTML = `${today.getFullYear()}`;
 lastModified.innerHTML = `Last Modified ${document.lastModified}`;
+
+document.addEventListener("DOMContentLoaded", () => {
+  const categoryLinks = document.querySelectorAll(".optionsof a");
+  const courseLinks = document.querySelectorAll(".optionscour li");
+
+  categoryLinks.forEach(link => {
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+
+      const category = link.textContent.trim(); // All, CSE, WDD
+
+      courseLinks.forEach(item => {
+        const courseCode = item.textContent.trim(); // Ej: CSE 110
+
+        // Mostrar todos
+        if (category === "All") {
+          item.style.display = "list-item";
+        } else {
+          // Mostrar solo los que comienzan con la categoría (ej: CSE)
+          if (courseCode.startsWith(category)) {
+            item.style.display = "list-item";
+          } else {
+            item.style.display = "none";
+          }
+        }
+      });
+    });
+  });
+});
