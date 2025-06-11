b48f1f9574ecd76bfad170dfeedbf6e721ef5471
brucemel <mel24004@byui.edu>
2025-06-10 22:52:28 -0500

    homepage

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,8 +1,6 @@
-const hamButton = document.querySelector('#menu');
+/* const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
-// const APIkey = 'e5a12d75cd9f19e5beaa30581b9f8e2a'
-// const city = 'Trujillo'
-// const url = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}'
+
 hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
@@ -50,16 +48,71 @@ const today = new Date();
 currentyear.innerHTML = `${today.getFullYear()}`;
 lastModified.innerHTML = `Last Modified ${document.lastModified}`;
 
-//     // async function GetWeather() {
-//     //     try {
-//     //         const response = await fetch(url);
-//     //         if (!response.ok) {
-//     //             throw new Error("No se pudo cargar el archivo JSON");
-//     //         }
-//     //         const data = await response.json();
-//     //         const celsius = data.temp./ 100
-//     //         console.log
-//     //     } catch (error) {
-//     //         console.error("Error al obtener los datos:", error);
-//     //     }
-// }
+ */
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+  navigation.classList.toggle('open');
+  hamButton.classList.toggle('open');
+});
+
+
+async function getMembers() {
+  try {
+    const response = await fetch("data/members.json");
+    if (!response.ok) {
+      throw new Error("Error al cargar JSON");
+    }
+    const data = await response.json();
+    displayMembers(data.members1);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+
+function displayMembers(members) {
+  const container = document.querySelector(".res-grid");
+  container.innerHTML = "";
+
+
+  const filteredMembers = members.filter(member =>
+    member.membershipLevel === 1 || member.membershipLevel === 2
+  );
+
+
+  const shuffledMembers = filteredMembers.sort(() => 0.5 - Math.random());
+
+
+  const numberOfMembers = Math.floor(Math.random() * 2) + 2; // 2 o 3
+  const selectedMembers = shuffledMembers.slice(0, numberOfMembers);
+
+
+  selectedMembers.forEach(member => {
+    const card = document.createElement("section");
+    card.classList.add("member-card");
+    card.innerHTML = `
+      <h3 class="elementofmem" id="titlemem">${member.name}</h3>
+      <p class="elementofmem">${member.description}</p>
+      <p class="elementofmem">${member.address}</p>
+      <p class="elementofmem">${member.phone}</p>
+      <p class="elementofmem">${member.membership}
+      <p class="elementofmem"><a href="${member.website}" target="_blank" id="emailofcom">${member.website}</a></p>
+      <img class="elementimgof" src="${member.logo}" alt="Logo de ${member.name}">
+    `;
+    container.appendChild(card);
+  });
+}
+
+
+getMembers();
+
+
+const currentyear = document.querySelector("#currentyear");
+const lastModified = document.getElementById("lastModified");
+
+const today = new Date();
+currentyear.innerHTML = `${today.getFullYear()}`;
+lastModified.innerHTML = `Last Modified: ${document.lastModified}`;
---
+++ chamber/scripts/weather.js
@@ -0,0 +1,76 @@
+const myTemperature = document.querySelector('#gradesinF');
+const myMood = document.querySelector('#moodofweather');
+const myImgwea = document.querySelector('#myimgwea');
+const myHigh = document.querySelector('#gradesinCH');
+const myLow = document.querySelector('#gradesinCL');
+const myHimidity = document.querySelector('#humiditywea');
+const mySunrise = document.querySelector('#sunmornig');
+const mySunset = document.querySelector('#suneven');
+
+const myTodaywea = document.querySelector('#gradesTo');
+const myWednesdaywea = document.querySelector('#gradesWe');
+const myThursdaywea = document.querySelector('#gradesTh');
+
+
+const myKey = "72ff68f674eff36ec01b3fd843e02251";
+const myLat = "-8.11198940289915";
+const myLong = "-79.02859361872879";
+
+const myURL = `https://api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`;
+const myURL1 = `https://api.openweathermap.org/data/2.5/forecast?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`;
+
+
+async function fetchCurrentWeather() {
+  try {
+    const response = await fetch(myURL);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      displayCurrentWeather(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayCurrentWeather(data) {
+  myTemperature.innerHTML = `${data.main.temp}&deg;F`;
+  myMood.innerHTML = data.weather[0].description;
+  const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  myImgwea.setAttribute('src', iconsrc);
+  myImgwea.setAttribute('alt', data.weather[0].description);
+  myHigh.innerHTML = `${data.main.temp_max}&deg;F`;
+  myLow.innerHTML = `${data.main.temp_min}&deg;F`;
+  myHimidity.innerHTML = `${data.main.humidity}%`;
+  mySunrise.innerHTML = new Date(data.sys.sunrise * 1000).toLocaleTimeString();
+  mySunset.innerHTML = new Date(data.sys.sunset * 1000).toLocaleTimeString();
+}
+
+async function fetchForecast() {
+  try {
+    const response = await fetch(myURL1);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      displayForecast(data);
+    } else {
+      throw Error(await response.text());
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function displayForecast(data) {
+  myTodaywea.innerHTML = `${data.list[0].main.temp}&deg;F`;
+  myWednesdaywea.innerHTML = `${data.list[8].main.temp}&deg;F`; // 
+  myThursdaywea.innerHTML = `${data.list[16].main.temp}&deg;F`; // 
+}
+
+fetchCurrentWeather();
+fetchForecast();
+
+
+
