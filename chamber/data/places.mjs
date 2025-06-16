88f7fbcd078717715c87bdfa0fcb804cc0f8a90e
brucemel <mel24004@byui.edu>
2025-06-15 22:07:48 -0500

    discoverpage

---
+++ chamber/data/places.mjs
@@ -0,0 +1,114 @@
+export const places = [
+    {
+        "name": "Supermercado Central",
+        "address": "Av. América 123, Trujillo",
+        "phone": "999 123 456",
+        "website": "https://supermercado.com",
+        "photo_url": "mercadocentral.webp",
+        "membershipLevel": 1,
+        "membership": "Gold Membership",
+        "description": "Supplier of food and household products.",
+        "moreinfo": "Trujillo's Central Market is a traditional market where you can find a variety of products, including fruits, vegetables, meat, juices, prepared foods, and clothing. It is located in the city's historic center, near the Chicago neighborhood. It is a popular place to buy fresh, affordable produce and is also a tourist attraction."
+
+    },
+    {
+        "name": "Trujillo Bakery",
+        "address": "Jr. Pizarro 789, Trujillo",
+        "phone": "988 765 432",
+        "website": "https://pantrujillo.com",
+        "photo_url": "panderiatrujillo.webp",
+        "membershipLevel": 2,
+        "membership": "Silver Membership",
+        "description": "Artisanal bakery with over 20 years of experience.",
+        "moreinfo": "Various bakery and pastry products are produced and baked, including breads, cakes, cookies, and other baked goods. Bakers are responsible for mixing ingredients, kneading, shaping dough, baking, and sometimes also decorating and presenting products. In addition to production, they are also responsible for quality control and cleaning work areas and equipment."
+    },
+    {
+        "name": "Libertad Hotel",
+        "address": "Calle Grau 456, Trujillo",
+        "phone": "977 654 321",
+        "website": "https://hotellibertad.com",
+        "photo_url": "hotellibertad.webp",
+        "membershipLevel": 3,
+        "membership": "Bronze Membership",
+        "description": "5-star hotel with rooms and event services.",
+        "moreinfo": "Hotel Libertad in Trujillo offers rooms with free Wi-Fi, air conditioning, cable TV, and a private bathroom. Some rooms have family options and are smoke-free. The hotel also features an outdoor pool, a restaurant, a bar/lounge, and free parking. It is located near tourist attractions such as Trujillo's Historic Center and the Huacas de Moche Museum."
+    },
+    {
+        "name": "Tech Store X",
+        "address": "Av. Larco 999, Trujillo",
+        "phone": "944 321 654",
+        "website": "https://tecnologiax.com",
+        "photo_url": "tiendax.webp",
+        "membershipLevel": 1,
+        "membership": "Gold Membership",
+        "description": "Electronics and computer accessories.",
+        "moreinfo": "You can find a wide range of electronics and accessories. Popular options include Tec Store and Coolbox inside the Mallplaza Trujillo, as well as Hiraoka and Compucenter. These stores offer everything from phones and computers to accessories and smart devices."
+    },
+    {
+        "name": "La Huaca Restaurant",
+        "address": "Jr. Ayacucho 111, Trujillo",
+        "phone": "987 654 321",
+        "website": "https://lahuaca.com",
+        "photo_url": "lahuaca.webp",
+        "membershipLevel": 2,
+        "membership": "Silver Membership",
+        "description": "Traditional and fusion cuisine.",
+        "moreinfo": "Offers a variety of dishes based on the rich Peruvian cuisine. This includes traditional dishes such as ceviche, lomo saltado, causa, and aji de gallina, as well as Nikkei (Peruvian-Japanese fusion) and Amazonian dishes. Additionally, many restaurants offer drinks such as pisco sour, chicha morada, and Peruvian craft beer. Some restaurants also specialize in signature cuisine, offering innovative and creative dishes."
+    },
+    {
+        "name": "XYZ Consulting",
+        "address": "Calle Los Laureles 222, Trujillo",
+        "phone": "933 123 789",
+        "website": "https://consultoraxyz.com",
+        "photo_url": "xyz.webp",
+        "membershipLevel": 3,
+        "membership": "Bronze Membership",
+        "description": "Legal and financial consulting services.",
+        "moreinfo": "A company specializing in professional accounting, finance, and tax consulting services, helping businesses and individuals organize their finances, comply with tax regulations, and make informed decisions. Its main functions include the preparation of financial statements, tax management, payroll processing, and financial planning advice."
+
+    },
+    {
+        "name": "FitPro Gym",
+        "address": "Av. España 777, Trujillo",
+        "phone": "911 111 222",
+        "website": "https://fitpro.com",
+        "photo_url": "fitpro.webp",
+        "membershipLevel": 1,
+        "membership": "Gold Membership",
+        "description": "Fitness and wellness center.",
+        "moreinfo": "A gym that focuses on providing personalized training and progress tracking to help you achieve your fitness goals. It offers a variety of features, such as access to workouts, class schedules, and notifications to keep you motivated. It can also refer to a fitness center with high-quality equipment and certified trainers."
+    },
+    {
+        "name": "Green Life Market",
+        "address": "Av. El Golf 150, Trujillo",
+        "phone": "900 456 789",
+        "website": "https://greenlifemarket.com",
+        "photo_url": "greenlife.webp",
+        "membershipLevel": 1,
+        "membership": "Gold Membership",
+        "description": "Organic products and eco-friendly goods.",
+        "moreinfo": "Health food store with a focus on organic, diet-friendly, and allergy-specific products, as well as prepared meal options and organic juices/smoothies. They have several locations in New Jersey, including Butler, Andover, and Morristown. The company was founded by the Yassin family in 2012, with the mission of making high-quality organic products accessible."
+    },
+    {
+        "name": "Andes Coffee",
+        "address": "Jr. Independencia 321, Trujillo",
+        "phone": "955 888 777",
+        "website": "https://andescoffee.com",
+        "photo_url": "andrescafe.webp",
+        "membershipLevel": 2,
+        "membership": "Silver Membership",
+        "description": "Local coffee shop with Peruvian beans.",
+        "moreinfo": "Sell ​​and prepare a variety of beverages and foods. They primarily offer coffee-based drinks, such as espresso, cappuccino, latte, and tea, as well as lighter options like herbal teas and cold drinks. Pastries and baked goods, such as croissants, cookies, cakes, and breads, are also commonly sold, along with snacks and sandwiches, such as sandwiches, salads, and wraps. Some establishments also offer fast food options or set meals."
+    },
+    {
+        "name": "SmartEdu Academy",
+        "address": "Calle Los Incas 456, Trujillo",
+        "phone": "933 456 987",
+        "website": "https://smartedu.com",
+        "photo_url": "smartedu.webp",
+        "membershipLevel": 3,
+        "membership": "Bronze Membership",
+        "description": "Courses and training for tech and languages.",
+        "moreinfo": "is a language school that offers online and in-person English and French courses. It stands out for its virtual platform with live classes, access to recordings, progress tracking, and teaching materials. It also operates as a certification center for international language exams."
+    }
+]
--- chamber/scripts/disco.js
+++ chamber/scripts/disco.js
@@ -1,13 +1,65 @@
+import { places } from '../data/places.mjs'
+console.log(places)
+
+const showHere = document.querySelector("#placeslima");
+const dialogBoxText = document.querySelector("#dialogBox div");
+
+function displayItems(places) {
+  places.forEach(x => {
+    const thecard = document.createElement('div')
+    const thephoto = document.createElement('img')
+    thephoto.src = `images/${x.photo_url}`
+    thephoto.alt = x.name
+    thecard.appendChild(thephoto)
+
+    const thetitle = document.createElement('h2')
+    thetitle.innerText = x.name
+    thecard.appendChild(thetitle)
+
+    const theaddress = document.createElement('address')
+    theaddress.innerText = x.address
+    thecard.appendChild(theaddress)
+
+    const thedesc = document.createElement('p')
+    thedesc.innerText = x.description
+    thecard.appendChild(thedesc)
+
+    const button = document.createElement('button');
+    button.innerText = "Learn More";
+    button.classList.add('learn-more-btn'); // puedes estilizarlo con CSS
+    button.addEventListener('click', () => {
+      dialogBox.showModal();
+      dialogBoxText.innerHTML = `${x.moreinfo}`
+      // O puedes redirigir a otra página o mostrar más detalles
+    });
+    thecard.appendChild(button);
+
+    closeButton.addEventListener("click", () => {
+      dialogBox.close();
+    });
+
+
+    showHere.appendChild(thecard)
+  });
+}
+
+displayItems(places)
+
+
+
+
 window.addEventListener('DOMContentLoaded', () => {
-  const sidebar = document.querySelector("#visit-message"); // Debes tener un contenedor con este id
+  const sidebar = document.querySelector("#visit-message");
 
   const lastVisit = localStorage.getItem("lastVisit");
   const now = Date.now();
 
   if (!lastVisit) {
+    // Primera vez que visita
     sidebar.textContent = "Welcome! Let us know if you have any questions.";
   } else {
-    const daysBetween = Math.floor((now - lastVisit) / (1000 * 60 * 60 * 24));
+    // Ya había visitado antes
+    const daysBetween = Math.floor((now - Number(lastVisit)) / (1000 * 60 * 60 * 24));
     if (daysBetween === 0) {
       sidebar.textContent = "Back so soon! Awesome!";
     } else if (daysBetween === 1) {
@@ -17,6 +69,7 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  // Actualizar la fecha de visita
+  // Guardar la fecha actual como última visita
   localStorage.setItem("lastVisit", now);
 });
+
