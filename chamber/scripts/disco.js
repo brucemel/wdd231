259d2dc71004565dacd4609c4e357abb5f29b071
brucemel <mel24004@byui.edu>
2025-06-15 22:17:49 -0500

    lazydis

--- chamber/scripts/disco.js
+++ chamber/scripts/disco.js
@@ -10,6 +10,7 @@ function displayItems(places) {
     const thephoto = document.createElement('img')
     thephoto.src = `images/${x.photo_url}`
     thephoto.alt = x.name
+    thephoto.loading = "lazy"
     thecard.appendChild(thephoto)
 
     const thetitle = document.createElement('h2')
