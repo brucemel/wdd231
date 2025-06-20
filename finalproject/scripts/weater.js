232f5cdf2fa683dccc3f263165ab345d026f0b4f
brucemel <mel24004@byui.edu>
2025-06-20 17:59:21 -0500

    coorectone

--- finalproject/scripts/weater.js
+++ finalproject/scripts/weater.js
@@ -19,52 +19,54 @@ const myURL = `https://api.openweathermap.org/data/2.5/weather?lat=${myLat}&lon=
 const myURL1 = `https://api.openweathermap.org/data/2.5/forecast?lat=${myLat}&lon=${myLong}&appid=${myKey}&units=imperial`;
 
 async function fetchCurrentWeather() {
-  try {
-    const response = await fetch(myURL);
-    if (response.ok) {
-      const data = await response.json();
-      // console.log(data);
-      displayCurrentWeather(data);
-    } else {
-      throw Error(await response.text());
+    try {
+        const response = await fetch(myURL);
+        if (response.ok) {
+            const data = await response.json();
+            // console.log(data);
+            displayCurrentWeather(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        // console.error(error);
     }
-  } catch (error) {
-    // console.error(error);
-  }
 }
 
 function displayCurrentWeather(data) {
-  myTemperature.innerHTML = `${data.main.temp}&deg;F`;
-  myMood.innerHTML = data.weather[0].description;
-  const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-  myImgwea.setAttribute('src', iconsrc);
-  myImgwea.setAttribute('alt', data.weather[0].description);
-  myHigh.innerHTML = `${data.main.temp_max}&deg;F`;
-  myLow.innerHTML = `${data.main.temp_min}&deg;F`;
-  myHimidity.innerHTML = `${data.main.humidity}%`;
-  mySunrise.innerHTML = new Date(data.sys.sunrise * 1000).toLocaleTimeString();
-  mySunset.innerHTML = new Date(data.sys.sunset * 1000).toLocaleTimeString();
+    myTemperature.innerHTML = `${data.main.temp}&deg;F`;
+    myMood.innerHTML = data.weather[0].description;
+
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    myimgwea.setAttribute('src', iconsrc);
+    myimgwea.setAttribute('alt', data.weather[0].description);
+
+    myHigh.innerHTML = `${data.main.temp_max}&deg;F`;
+    myLow.innerHTML = `${data.main.temp_min}&deg;F`;
+    myHimidity.innerHTML = `${data.main.humidity}%`;
+    mySunrise.innerHTML = new Date(data.sys.sunrise * 1000).toLocaleTimeString();
+    mySunset.innerHTML = new Date(data.sys.sunset * 1000).toLocaleTimeString();
 }
 
 async function fetchForecast() {
-  try {
-    const response = await fetch(myURL1);
-    if (response.ok) {
-      const data = await response.json();
-      // console.log(data);
-      displayForecast(data);
-    } else {
-      throw Error(await response.text());
+    try {
+        const response = await fetch(myURL1);
+        if (response.ok) {
+            const data = await response.json();
+            // console.log(data);
+            displayForecast(data);
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        // console.error(error);
     }
-  } catch (error) {
-    // console.error(error);
-  }
 }
 
 function displayForecast(data) {
-  myTodaywea.innerHTML = `${data.list[0].main.temp}&deg;F`;
-  myWednesdaywea.innerHTML = `${data.list[8].main.temp}&deg;F`;
-  myThursdaywea.innerHTML = `${data.list[16].main.temp}&deg;F`;
+    myTodaywea.innerHTML = `${data.list[0].main.temp}&deg;F`;
+    myWednesdaywea.innerHTML = `${data.list[8].main.temp}&deg;F`;
+    myThursdaywea.innerHTML = `${data.list[16].main.temp}&deg;F`;
 }
 
 fetchCurrentWeather();
