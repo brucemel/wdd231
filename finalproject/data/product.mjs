4f16ef850bb0ce924c6c0ed4a6b72d1ce5a68057
brucemel <mel24004@byui.edu>
2025-06-20 21:34:01 -0500

    arraystool

--- finalproject/data/product.mjs
+++ finalproject/data/product.mjs
@@ -125,7 +125,7 @@ export const product = [
     "address": "Warehouse in Trujillo",
     "phone": "Quality: Plastic",
     "website": "https://tinyurl.com/our-plas",
-    "photo_url": "stoolplas.webp",
+    "photo_url": "stoolplastic.webp",
     "membershipLevel": 3,
     "membership": "Also available in Chimbote",
     "description": "Long-lasting and reliable.",
